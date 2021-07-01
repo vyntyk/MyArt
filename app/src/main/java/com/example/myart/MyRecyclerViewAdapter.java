cca023e3f7fa90a6155fd634610df790374e0d82
Konstantin Belousov <belousovconst@gmail.com>
2021-07-01 19:57:11 +0300

    review

--- app/src/main/java/com/example/myart/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/myart/MyRecyclerViewAdapter.java
@@ -5,45 +5,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
-public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder> {
 
     private List<String> mData;
-    private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
 
     // data is passed into the constructor
-    public MyRecyclerViewAdapter(Context context, List<String> data) {
-        this.mInflater = LayoutInflater.from(context);
+    public MyRecyclerViewAdapter(List<String> data) {
         this.mData = data;
     }
-    // inflates the row layout from xml when needed
+
+    @NonNull
+    @NotNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.fragment_home, parent, false);
-        return new ViewHolder(view);
+    public MyViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_list_item, parent, false));
     }
-    // binds the data to the TextView in each row
+
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull @NotNull MyViewHolder holder, int position) {
         String animal = mData.get(position);
         holder.myTextView.setText(animal);
     }
-    // total number of rows
+
     @Override
     public int getItemCount() {
         return mData.size();
     }
-
     // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView myTextView;
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        ViewHolder(View itemView) {
+        public TextView myTextView;
+
+        MyViewHolder(View itemView) {
             super(itemView);
-            myTextView = itemView.findViewById(R.id.recycler);
+            myTextView = itemView.findViewById(R.id.title);
             itemView.setOnClickListener(this);
         }
         @Override
@@ -51,14 +55,14 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
             if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
         }
     }
-    // convenience method for getting data at click position
-    public String getItem(int id) {
-        return mData.get(id);
-    }
-    // allows clicks events to be caught
-    public void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
+//    // convenience method for getting data at click position
+//    public String getItem(int id) {
+//        return mData.get(id);
+//    }
+//    // allows clicks events to be caught
+//    public void setClickListener(ItemClickListener itemClickListener) {
+//        this.mClickListener = itemClickListener;
+//    }
     // parent activity will implement this method to respond to click events
     public interface ItemClickListener {
         void onItemClick(View view, int position);
--- app/src/main/java/com/example/myart/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myart/ui/home/HomeFragment.java
@@ -22,35 +22,45 @@ import com.example.myart.R;
 import com.example.myart.repository.Repository;
 import com.example.myart.repository.impl.RepositoryImpl;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class HomeFragment extends Fragment implements MyRecyclerViewAdapter.ItemClickListener{
 
     private HomeViewModel homeViewModel;
-    RecyclerView recyclerView;
-    MyRecyclerViewAdapter adapter;
+    private MyRecyclerViewAdapter adapter;
     private FrameLayout container;
     private ConstraintLayout constraintLayout;
 
     @Override
     public void onItemClick(View view, int position) {
-        Toast.makeText(getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Repository<String> repository = new RepositoryImpl();
+        List<String> data = repository.getData().stream().collect(Collectors.toList());
+        adapter = new MyRecyclerViewAdapter(data);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Repository<String> repository = new RepositoryImpl();
-        List<String> data = repository.getData().stream().collect(Collectors.toList());
-
         RecyclerView recyclerView = root.findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new MyRecyclerViewAdapter(getContext(), data);
-        adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
+//        adapter.setClickListener(this);
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
