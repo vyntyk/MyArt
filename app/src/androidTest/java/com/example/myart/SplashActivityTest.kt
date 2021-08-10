f4d78b6ad20127114cf9a74f4ceceb8a03d7a652
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-08-10 21:54:22 +0300

    Корутины в Сплеш

---
+++ app/src/androidTest/java/com/example/myart/SplashActivityTest.kt
@@ -0,0 +1,24 @@
+package com.example.myart
+
+import junit.framework.TestCase
+
+class SplashActivityTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testAttachBaseContext() {}
+
+    fun testOnCreate() {}
+
+    fun testSetTheme() {}
+
+    fun testGetActivityScope() {}
+
+    fun testTestOnCreate() {}
+
+    fun testOnPause() {}
+}
--- app/src/main/java/com/example/myart/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/myart/ui/notifications/NotificationsViewModel.kt
@@ -5,12 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class NotificationsViewModel : ViewModel() {
-    private val mText: MutableLiveData<String?>
-    val text: LiveData<String?>
-        get() = mText
 
-    init {
-        mText = MutableLiveData()
-        mText.value = "This is notifications fragment"
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
     }
+    val text: LiveData<String> = _text
 }
